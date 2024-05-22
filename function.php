b5effaac775f8f9161b3ff08e6d004b8be1a6736
wmancaniello <wmancaniello@gmail.com>
2024-05-22 17:01:00 +0200

    add function.php

---
+++ function.php
@@ -0,0 +1,5 @@
+<?php
+function validateEmail($email)
+{
+    return preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email);
+}
--- index.php
+++ index.php
@@ -1,11 +1,38 @@
+<?php
+include './function.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Newsletter Subscription</title>
+    <title>Newsletter subscription</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    
+    <div class="container mt-5">
+        <h1 class="text-center">Iscrizione alla newsletter</h1>
+
+        <form action="" method="post" class="mt-4 d-flex flex-column gap-3">
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Inserisci la tua email" required>
+
+
+                <?php if (isset($_POST['email']) && !validateEmail($_POST['email'])) : ?>
+                    <small class="text-danger">Email non valida, esempio: "pietrorossi@email.com"</small>
+                <?php endif; ?>
+
+            </div>
+            <button type="submit" class="btn btn-primary">Registrati</button>
+        </form>
+
+    </div>
+
+
 </body>
+
 </html>
